ebfc8760d3acdc2ad41c86713a881815f8ae8ea9
heidysnj <heidysnunez15@gmail.com>
2021-03-19 23:26:26 +0000

    inmigration array  in flux.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,30 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			 inmigration: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+		addInmigration(name, phone, email, address) {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "post",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						email: email,
+						agenda_slug: "heidysAgenda"
+					})
+				}).then(() => {
+					fetch("https://assets.breatheco.de/apis/fake/contact/agenda/heidysAgenda")
+						.then(response => response.json())
+						.then(result => {
+							setStore({
+								inmigration: result
+							});
+						})
+						.catch(e => console.error(e));
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
