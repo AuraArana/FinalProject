6aee330ad7a98ddb0b05da81940db334f1be5715
jafinol <jafinol@gmail.com>
2021-03-23 01:23:00 +0000

    Jose Function6

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -73,6 +73,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 					});
             },
 
+            getjose6: () => {
+				fetch(url + "agenda/aura2")
+					.then(res => res.json())
+					.then(response => {
+						//console.log(response);
+						setStore({ services: response });
+					});
+            },
             getAura6: () => {
 				fetch(url + "agenda/aura2")
 					.then(res => res.json())
